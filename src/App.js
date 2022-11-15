ffe85dd73a097f1dc08c44ec7f06d51184cd0479
kimhsr <kimhsr@naver.com>
2022-11-16 00:38:38 +0900

    state, 데이터바인딩

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 라면 맛집';
+  let [title, b] = useState(['남자 코트 추천', '강남 라면 맛집', '리액트 독학']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color : 'red', fontSize : '16px'}}>블로그</h4>
+        <h4>블로그</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{ title[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
