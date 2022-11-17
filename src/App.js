cb5ded4cd95760a874d577a34e21bc6496a441fe
kimhsr <kimhsr@naver.com>
2022-11-17 15:03:29 +0900

    반복문 map 연습

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import { useState } from 'react';
 function App() {
 
   let [title, titleChange] = useState(['남자 코트 추천', '강남 라면 맛집', '리액트 독학']);
-  let [like, likeChange] = useState(0);
+  let [like, likeChange] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
-  function modalEvent(){
-    setModal(modal === true ? false : true)   
+  function modalEvent() {
+    setModal(modal === true ? false : true)
   }
 
   return (
@@ -20,28 +20,43 @@ function App() {
         <h4>블로그</h4>
       </div>
 
-      <button onClick={ ()=>{
+      <button onClick={() => {
         let titleCopy = [...title];
         titleCopy[0] = '여자 코트 추천';
         titleChange(titleCopy);
-        } }>글 제목 수정</button>
+      }}>글 제목 수정</button>
 
-      <div className='list'>
-        <h4> { title[0] } <span onClick={ ()=>{
-          likeChange(like + 1) 
-          } }>👍</span> { like } </h4>
+      {/* <div className='list'>
+        <h4> {title[0]} <span onClick={() => {
+          likeChange(like + 1)
+        }}>👍</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className='list'>
-        <h4>{ title[1] }</h4>
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className='list'>
-        <h4 onClick={modalEvent}>{ title[2] }</h4>
+        <h4 onClick={modalEvent}>{title[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        title.map(function (a, i) {
+          return (
+            <div className='list' key={i}>
+              <h4>{title[i]} <span onClick={() => {
+                let likeCopy = [...like];
+                likeCopy[i] = likeCopy[i] + 1;
+                likeChange(likeCopy);
+              }}>👍</span> {like[i]}</h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
 
       {
         modal === true ? <Modal></Modal> : null
@@ -51,13 +66,13 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal() {
   return (
-      <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
